4addd16af8e0ccb2349c6289c660a24678961273
Aaron <petrekaj@gmail.com>
2022-11-28 16:37:00 -0800

    Drawing the Crosshairs

--- Source/Blaster/Public/HUD/BlasterHUD.h
+++ Source/Blaster/Public/HUD/BlasterHUD.h
@@ -37,6 +37,8 @@ private:
 
 	FHUDPackage HUDPackage;
 
+	void DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter);
+
 public:
 
 	FORCEINLINE void SetHUDPackage(const FHUDPackage& Package) { HUDPackage = Package; }
